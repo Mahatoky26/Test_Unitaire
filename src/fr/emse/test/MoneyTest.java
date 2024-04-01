abc078ad6333b7d2cb09a5ffb880ddd7c3ebc197
Mahatoky26 <matokyjos26@gmail.com>
2024-04-01 22:15:21 +0300

    2eme question test_U

--- src/fr/emse/test/MoneyTest.java
+++ src/fr/emse/test/MoneyTest.java
@@ -1,13 +1,13 @@
 package fr.emse.test;
-
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 public class MoneyTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+@Test
+public void testSimpleAdd() {
+		Money m12CHF = new Money(12, "CHF"); // création de données
+		Money m14CHF = new Money(14, "CHF");
+		Money expected = new Money(26, "CHF");
+		Money result = m12CHF.add(m14CHF); // exécution de la méthode testée
+		assertTrue(expected.equals(result)); // comparaison
+}
 }
